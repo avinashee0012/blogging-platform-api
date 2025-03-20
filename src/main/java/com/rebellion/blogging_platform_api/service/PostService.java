30670aa54d14d63b27b3215922ddcbc0309850db
Rebellion <avinashee0012@gmail.com>
2025-03-20 12:31:07 +0530

    Created Service Logics

--- src/main/java/com/rebellion/blogging_platform_api/service/PostService.java
+++ src/main/java/com/rebellion/blogging_platform_api/service/PostService.java
@@ -7,9 +7,10 @@ import com.rebellion.blogging_platform_api.dao.PostDao;
 import com.rebellion.blogging_platform_api.entity.Post;
 
 public interface PostService {
-    ResponseEntity<Post> createPost(PostDao postDao);
-    ResponseEntity<List<Post>> getPosts(String term);
-    ResponseEntity<Post> getPostById(int id);
-    ResponseEntity<Post> updatePostById(int id, PostDao postDao);
-    ResponseEntity<String> deletePostById(int id);
+    ResponseEntity<?> createPost(Post post);
+    ResponseEntity<List<Post>> getPosts();
+    ResponseEntity<List<Post>> getPostsBySearchTerm(String term);
+    ResponseEntity<?> getPostById(int id);
+    ResponseEntity<?> updatePostById(int id, PostDao postDao);
+    ResponseEntity<?> deletePostById(int id);
 }
--- src/main/java/com/rebellion/blogging_platform_api/service/PostServiceImpl.java
+++ src/main/java/com/rebellion/blogging_platform_api/service/PostServiceImpl.java
@@ -1,12 +1,16 @@
 package com.rebellion.blogging_platform_api.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.rebellion.blogging_platform_api.dao.PostDao;
+import com.rebellion.blogging_platform_api.entity.Status;
+import com.rebellion.blogging_platform_api.entity.StatusEnum;
 import com.rebellion.blogging_platform_api.entity.Post;
 import com.rebellion.blogging_platform_api.repo.PostRepo;
 
@@ -20,33 +24,53 @@ public class PostServiceImpl implements PostService{
     }
 
     @Override
-    public ResponseEntity<Post> createPost(PostDao postDao) {
-        // TODO SERVICE createPost(PostDao postDao)
-        return null;
+    public ResponseEntity<Post> createPost(Post post) {
+        Post savedPost = postRepo.save(post);
+        return new ResponseEntity<>(savedPost, HttpStatus.CREATED);
     }
 
     @Override
-    public ResponseEntity<List<Post>> getPosts(String term) {
-        // TODO SERVICE getPosts(String term)
+    public ResponseEntity<List<Post>> getPosts() {
         return new ResponseEntity<>(postRepo.findAll(), HttpStatus.FOUND);
     }
 
     @Override
-    public ResponseEntity<Post> getPostById(int id) {
-        // TODO SERVICE getPostById(int id)
-        return null;
+    public ResponseEntity<List<Post>> getPostsBySearchTerm(String term) {
+        // TODO SERVICE Implement getPostsBySearchTerm(String term)
+        
     }
 
     @Override
-    public ResponseEntity<Post> updatePostById(int id, PostDao postDao) {
-        // TODO SERVICE updatePostById(int id, PostDao postDao)
-        return null;
+    public ResponseEntity<?> getPostById(int id) {
+        Optional<Post> post = postRepo.findById(id);
+        if(post.isPresent()) {
+            return new ResponseEntity<>(post, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new Status(StatusEnum.ERROR, HttpStatus.NOT_FOUND, "No post found with Id: " + id), HttpStatus.NOT_FOUND);
     }
 
     @Override
-    public ResponseEntity<String> deletePostById(int id) {
-        // TODO SERVICE deletePostById(int id)
-        return null;
+    public ResponseEntity<?> updatePostById(int id, PostDao postDao) {
+        Optional<Post> post = postRepo.findById(id);
+        if(post.isPresent()) {
+            post.get().setTitle(postDao.getTitle());
+            post.get().setContent(postDao.getContent());
+            post.get().setCategory(postDao.getCategory());
+            post.get().setTags(postDao.getTags());
+            post.get().setUpdatedAt(LocalDateTime.now());
+            return new ResponseEntity<>(postRepo.save(post.get()), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new Status(StatusEnum.ERROR, HttpStatus.NOT_FOUND, "No post found with Id: " + id), HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    public ResponseEntity<?> deletePostById(int id) {
+        Optional<Post> post = postRepo.findById(id);
+        if(post.isPresent()) {
+            postRepo.deleteById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(new Status(StatusEnum.ERROR, HttpStatus.NOT_FOUND, "No post found with Id: " + id), HttpStatus.NOT_FOUND);
     }
 
 }
